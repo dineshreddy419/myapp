1c2d7edcd9c932cb62238ed24c45d9cdfcab4c63
Admin <Admin@192.168.0.7>
2019-05-10 09:12:34 +0530

    Commit Operation

---
+++ src/com/app/model/Student.java
@@ -0,0 +1,35 @@
+package com.app.model;
+
+public class Student {
+
+	private int stdId;
+	
+	private String stdName;
+
+	public Student() {
+		super();
+	}
+
+	public int getStdId() {
+		return stdId;
+	}
+
+	public void setStdId(int stdId) {
+		this.stdId = stdId;
+	}
+
+	public String getStdName() {
+		return stdName;
+	}
+
+	public void setStdName(String stdName) {
+		this.stdName = stdName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [stdId=" + stdId + ", stdName=" + stdName + "]";
+	}
+	
+	
+}
